e37cb03ac80d936dc5bcc7970919ab7ff105aeb2
riwoo0902 <imliu4342@gmail.com>
2026-07-23 19:37:33 +0900

    save

--- Assets/Lrw/Script/Agent/HealthSystem/IHealthModule.cs
+++ Assets/Lrw/Script/Agent/HealthSystem/IHealthModule.cs
@@ -3,6 +3,8 @@ namespace Lrw.Script.Agent.HealthSystem
     public interface IHealthModule
     {
         float CurrentHealth { get; set; }
+        void TakeDamage(float damage);
+        void Heal(float heal);
         event HealthChangedHandler OnHealthChanged;
     }
 }
