429b309215855f245eb7645dd1cff77a628f410b
riwoo0902 <imliu4342@gmail.com>
2026-07-23 12:14:48 +0900

    save

--- Assets/Lrw/Script/StatSystem/StatDataSo.cs
+++ Assets/Lrw/Script/StatSystem/StatDataSo.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 namespace Lrw.Script.StatSystem
 {
-    [CreateAssetMenu(fileName = "StatSo", menuName = "Agent/StatSystem/", order = 0)]
+    [CreateAssetMenu(fileName = "Stat Data So", menuName = "Agent/StatSystem/Stat Data So", order = 0)]
     public class StatDataSo : ScriptableObject
     {
         [field:SerializeField] public Sprite StatIcon{get; private set;}
-        
+        [field:SerializeField] public string StatName { get; private set; }
         [field:SerializeField] public float MinValue{get; private set;}
         [field:SerializeField] public float MaxValue{get; private set;}
         
