a5b085b2aa722f6944cd7374f02ab27082bf98c7
riwoo0902 <imliu4342@gmail.com>
2026-07-25 03:20:53 +0900

    save

---
+++ Assets/Lrw/Script/UI/SFXPlayer.cs
@@ -0,0 +1,19 @@
+﻿using DevLib.EventChannelSystem;
+using DevLib.SoundSystem.Runtime;
+using UnityEngine;
+
+namespace Lrw.Script.UI
+{
+    public class SFXPlayer : MonoBehaviour
+    {
+        [SerializeField] private SoundClipSO soundClip;
+        
+        [ContextMenu("Play")]
+        public void Play()
+        {
+            EventBus<PlaySoundEvents>.Invoke(SoundEvents.PlaySound.Init(transform.position, soundClip));
+        }
+        
+        
+    }
+}
