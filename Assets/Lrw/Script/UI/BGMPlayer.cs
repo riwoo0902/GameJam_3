03af457ec6ab30cc6f70a812ab05a131c4533fba
riwoo0902 <imliu4342@gmail.com>
2026-07-25 05:15:54 +0900

    feat : soundSo

---
+++ Assets/Lrw/Script/UI/BGMPlayer.cs
@@ -0,0 +1,16 @@
+﻿using DevLib.EventChannelSystem;
+using DevLib.SoundSystem.Runtime;
+using UnityEngine;
+
+namespace Lrw.Script.UI
+{
+    public class BGMPlayer : MonoBehaviour
+    {
+        [SerializeField] private SoundClipSO soundClip;
+        
+        public void Awake()
+        {
+            EventBus<PlaySoundEvents>.Invoke(SoundEvents.PlaySound.Init(transform.position, soundClip,1));
+        }
+    }
+}
