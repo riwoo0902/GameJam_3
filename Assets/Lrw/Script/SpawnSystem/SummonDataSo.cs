ce3f0b8a504b41e183d9ad98020fd20d68fa12d6
riwoo0902 <imliu4342@gmail.com>
2026-07-24 16:53:18 +0900

    save

---
+++ Assets/Lrw/Script/SpawnSystem/SummonDataSo.cs
@@ -0,0 +1,21 @@
+﻿using LrwLib.UnityPosition;
+using UnityEngine;
+
+namespace Lrw.Script.SpawnSystem
+{
+    [CreateAssetMenu(fileName = "Spawn Data", menuName = "Spawn Data", order = 0)]
+    public class SummonDataSo : ScriptableObject
+    {
+        [field:SerializeField] public UnityPos Pos { get; private set; }
+        [field:SerializeField] public GameObject Prefab { get; private set; }
+        [field: SerializeField] public int Count { get; private set; } = 1;
+
+        [field:SerializeField] public float RandomRange { get; private set; } = 1;
+        [field:SerializeField] public float StartDelay { get; private set; } = 0;
+        
+        [Header("Loop")]
+        [field: SerializeField] public bool Loop { get; private set; } = false;
+        [field: SerializeField] public int LoopDelay { get; private set; } = 0;
+        
+    }
+}
---
+++ Assets/Lrw/Script/SpawnSystem/Summoner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using DevLib.ObjectPool.Runtime;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Lrw.Script.SpawnSystem
+{
+    public class Summoner : MonoBehaviour
+    {
+        [SerializeField] private SummonDataSo[] summonDataList;
+        
+        private void Awake()
+        {
+            Summon();
+        }
+
+        private void Summon()
+        {
+            foreach (SummonDataSo summonData in summonDataList)
+            {
+                StartCoroutine(SummonCoroutine(summonData));
+            }
+        }
+
+        private IEnumerator SummonCoroutine(SummonDataSo summonData)
+        {
+            yield return new WaitForSeconds(summonData.StartDelay);
+
+            do
+            {
+                for (int i = 0; i < summonData.Count; i++)
+                {
+                    GameObject go = Instantiate(summonData.Prefab);
+                    go.transform.position = (Vector2)(Vector3)summonData.Pos + Random.insideUnitCircle * summonData.RandomRange;
+                }
+                yield return new WaitForSeconds(summonData.LoopDelay);
+            } while (summonData.Loop);
+        }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            if(summonDataList == null) return;
+            
+            Gizmos.color = Color.green;
+            
+            foreach (SummonDataSo summonData in summonDataList) Gizmos.DrawWireSphere(summonData.Pos, Mathf.Max(summonData.RandomRange,0.3f));
+        }
+        
+        
+        
+    }
+}
